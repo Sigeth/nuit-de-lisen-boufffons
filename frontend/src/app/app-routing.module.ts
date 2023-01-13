cc753901d82911e1587a48d96306aba9ffce8960
Maxou <Jackson.Kyoshi@gmail.com>
2023-01-13 21:22:04 +0100

    feat:component for reservation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccueilComponent} from "./component/accueil/accueil.component";
+import { ReservMainComponent } from './component/reservation/reserv-main/reserv-main.component';
 
 const routes: Routes = [
-  {path: '', component: AccueilComponent}
+  {path: '', component: AccueilComponent},
+  {path: 'reservation', component: ReservMainComponent}
 ];
 
 @NgModule({
