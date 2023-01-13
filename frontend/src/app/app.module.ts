fa4831504d478fa409849b457c6a82a9f6dd13d1
Sigeth <52868615+Sigeth@users.noreply.github.com>
2023-01-13 20:27:16 +0100

    fix: Hfooter deleted

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { HfooterComponent } from './component/hfooter/hfooter.component';
 import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HfooterComponent,
     FooterComponent
   ],
   imports: [
