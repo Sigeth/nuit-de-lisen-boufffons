921e6345ba009321e8598a5bb1d5eb653a0d7e68
Bastientt <98900238+Bastientt@users.noreply.github.com>
2023-01-13 22:25:05 +0100

    feat: add reservation pages

--- frontend/src/app/component/accueil/accueil.component.ts
+++ frontend/src/app/component/accueil/accueil.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent {
-
+  condition:boolean = false ;
+  changeCondition():void{
+    this.condition=!this.condition;
+  }
 }
